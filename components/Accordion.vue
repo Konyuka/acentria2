6fc69a613549fdbd3f01f55ce380e27859b55ae1
Konyuka <michaelsaiba84@gmail.com>
2024-10-07 17:09:47 +0300

    pricing

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -3,10 +3,22 @@ const props = defineProps({
     faqs: Array,
 })  
 
+import { defineComponent, h, ref } from 'vue'
+import {
+    Dialog,
+    DialogPanel,
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    RadioGroup,
+    RadioGroupOption,
+} from '@headlessui/vue'
+import { Bars3Icon, MinusSmallIcon, PlusSmallIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { CheckIcon } from '@heroicons/vue/20/solid'
 </script>
 
 <template>
-    <div class="bg-white px-4 py-3 md:px-16 md:pb-4 md:pt-0 lg:pb-2">
+    <div class="bg-white px-4 py-3 md:px-16 md:pb-4 md:pt-0 lg:pb-2 font-sans">
         <div class="max-w-7xl mx-auto pb-4">
             <!-- <div class="pb-5 text-center">
                 <h2
@@ -14,7 +26,33 @@ const props = defineProps({
                     Frequently Asked Questions
                 </h2>
             </div> -->
-            <div class="space-y-5 grid grid-cols-1 md:grid-cols-2 gap-2">
+
+            <!-- FAQ section -->
+            <div class="mx-auto my-2 max-w-7xl px-6 lg:px-8">
+                <div class="mx-auto max-w-6xl divide-y divide-gray-900/10">
+                    <!-- <h2 class="text-2xl font-bold leading-10 tracking-tight text-gray-900">Frequently asked questions
+                    </h2> -->
+                    <dl class="mt-10 space-y-6 divide-y divide-gray-900/10">
+                        <Disclosure as="div" v-for="faq in faqs" :key="faq.name" class="pt-6" v-slot="{ open }">
+                            <dt>
+                                <DisclosureButton
+                                    class="flex w-full items-start justify-between text-left text-gray-900">
+                                    <span class="text-base font-semibold leading-7">{{ faq.name }}</span>
+                                    <span class="ml-6 flex h-7 items-center">
+                                        <PlusSmallIcon v-if="!open" class="h-6 w-6" aria-hidden="true" />
+                                        <MinusSmallIcon v-else class="!text-brand-primary h-6 w-6" aria-hidden="true" />
+                                    </span>
+                                </DisclosureButton>
+                            </dt>
+                            <DisclosurePanel as="dd" class="mt-2 pr-12">
+                                <p class="text-base leading-7 text-gray-600">{{ faq.content }}</p>
+                            </DisclosurePanel>
+                        </Disclosure>
+                    </dl>
+                </div>
+            </div>
+
+            <!-- <div class="space-y-5 grid grid-cols-1 md:grid-cols-2 gap-2">
 
                 <article v-for="faq in faqs" class="relative isolate !mt-2">
                     <div class="w-full">
@@ -33,7 +71,7 @@ const props = defineProps({
                     </div>
                 </article>
 
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
