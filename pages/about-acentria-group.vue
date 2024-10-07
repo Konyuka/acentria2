3d54be4fbbb52dc27d6f4548a8771ea827f190cf
Konyuka <michaelsaiba84@gmail.com>
2024-10-07 20:31:59 +0300

    block quotes

--- pages/about-acentria-group.vue
+++ pages/about-acentria-group.vue
@@ -9,7 +9,7 @@ const InsuranceBannerContent = ref({
 
 const mission = ref(null)
 // const selectedMenu = ref(null)
-const selectedMenu = computed(()=>{
+const selectedMenu = computed(() => {
     return mission.value?.currentMenu
 })
 
@@ -51,7 +51,7 @@ onMounted(() => {
 
                             <div data-v-02281a80="" class="font-sans py-2 text-white"
                                 style="background-color: rgb(246, 248, 252)">
-                                
+
                                 <div class="container mx-auto flex px-4 lg:flex-row flex-col my-8 lg:my-4 lg:gap-16"
                                     data-v-inspector="components/Why.vue:8:9">
                                     <div class="relative py-4 lg:basis-1/2 scale-90"
@@ -234,7 +234,7 @@ onMounted(() => {
 
                                     <div class="grid grid-cols-1 md:grid-cols-1 gap-5 mt-4">
                                         <div class="font-sans content-center items-center grid grid-cols-1 gap-5">
-                                            <div v-show="selectedMenu=='vision'"
+                                            <div v-show="selectedMenu == 'vision'"
                                                 class="flex flex-row gap-2 justify-center">
 
                                                 <div class="mt-10 container mx-auto flex px-4 lg:flex-row flex-col lg:gap-0 justify-around"
@@ -661,41 +661,53 @@ onMounted(() => {
 
                         </div>
                     </div>
-                    
 
-                    <div class="bg-gray-900 font-sans">
-
-                        <div class="relative isolate overflow-hidden pt-5 py-10">
-                            <img src="https://images.pexels.com/photos/1108572/pexels-photo-1108572.jpeg?auto=compress&cs=tinysrgb&w=600"
-                                alt="" class="absolute inset-0 -z-20 h-full w-full object-cover">
-                            <div class="absolute inset-0 bg-black opacity-60 -z-10"></div>
-                            <div class="mx-auto max-w-6xl py-10 !z-50">
-
-                                <div class="justify-center text-center !z-50">
-                                    <h1
-                                        class="text-balance text-4xl font-bold tracking-tight text-brand-primary sm:text-6xl">
-                                        <span class="text-white">Sustainability &</span> Corporate Social Responsibility
-                                    </h1>
-                                    <p class="mt-6 text-sm leading-8 text-white sm:mx-40">
-
-                                        Our initiatives focus on reducing our environmental impact, promoting ethical
-                                        business practices,
-                                        and contributing positively to the communities we serve. We believe in creating
-                                        long-term value
-                                        for our stakeholders by integrating sustainable practices into our core
-                                        operations and fostering
-                                        a culture of responsibility and transparency.
-                                    </p>
-                                    <div class="mt-10 flex items-center justify-center gap-x-6">
-                                        <NuxtLink href="/csr"
-                                            class="rounded-md bg-brand-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">
-                                            Learn More <i class="fas fa-arrow-right ml-2"></i>
-                                        </NuxtLink>
-
-                                    </div>
-                                </div>
+                    <div class="mx-auto my-10 max-w-7xl sm:px-6 lg:px-8">
+                        <div
+                            class="relative overflow-hidden bg-gray-900 px-6 py-20 shadow-xl sm:rounded-3xl sm:px-10 sm:py-24 md:px-12 lg:px-20">
+                            <img class="absolute inset-0 h-full w-full object-cover brightness-150 saturate-0"
+                                src="https://images.pexels.com/photos/1108572/pexels-photo-1108572.jpeg?auto=compress&cs=tinysrgb&w=600"
+                                alt="" />
+                            <div class="absolute inset-0 bg-gray-900/90 mix-blend-multiply" />
+                            <div class="absolute -left-80 -top-56 transform-gpu blur-3xl" aria-hidden="true">
+                                <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#ff4694] to-[#776fff] opacity-[0.45]"
+                                    style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
                             </div>
-
+                            <div class="hidden md:absolute md:bottom-16 md:left-[50rem] md:block md:transform-gpu md:blur-3xl"
+                                aria-hidden="true">
+                                <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#ff4694] to-[#776fff] opacity-25"
+                                    style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+                            </div>
+                            <div class="relative mx-auto max-w-2xl lg:mx-0">
+                                <h1 class="text-white font-bold text-3xl">
+                                    Corporate Social Responsibility
+                                    <br>
+                                    <span class="text-brand-primary">&
+                                        Sustainability
+                                    </span>
+                                </h1>
+                                <figure>
+                                    <blockquote class="mt-6 text-lg font-semibold text-white sm:text-xl sm:leading-8">
+                                        <p>
+                                            Our initiatives focus on reducing our environmental impact, promoting
+                                            ethical
+                                            business practices,
+                                            and contributing positively to the communities we serve. We believe in
+                                            creating
+                                            long-term value
+                                            for our stakeholders by integrating sustainable practices into our core
+                                            operations and fostering
+                                            a culture of responsibility and transparency.
+                                        </p>
+                                    </blockquote>
+                                    <figcaption class="mt-6 text-base text-white">
+                                        <NuxtLink to="/csr"
+                                            class="underline underline-offset-8 hover:button-animation font-semibold tracking-widest">
+                                            Learn More <i class="fas fa-caret-right"></i>
+                                        </NuxtLink>
+                                    </figcaption>
+                                </figure>
+                            </div>
                         </div>
                     </div>
 
