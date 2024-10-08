83cd8fb772d659f14579cd2b34dbd531943ef31e
Konyuka <michaelsaiba84@gmail.com>
2024-10-08 19:57:52 +0300

    push

--- pages/index.vue
+++ pages/index.vue
@@ -306,32 +306,7 @@ onMounted(() => {
 
               <Awards />
 
-              <div class="bg-white !font-sans container my-5">
-                <div class="mx-auto max-w-7xl py-5 sm:px-2 lg:px-4">
-                  <div class="mx-auto max-w-7xl px-4 lg:max-w-none">
-                    <div class="max-w-7xl justify-between content-center">
-                      <h2 class="text-3xl font-semibold tracking-tight text-gray-900 text-center">
-                        Join Our Team
-                      </h2>
-                    </div>
-                    <div class="mt-5 grid grid-cols-1 gap-x-2 gap-y-4 lg:grid-cols-3 max-w-6xl mx-auto">
-                      <div v-for="member in joinAsMembers" :key="member.name"
-                        class="group button-animation sm:flex lg:block bg-gray-50 px-5 py-2 rounded-xs border-t-8  border-brand-primary">
-                        <div class="sm:flex-shrink-0 flex justify-center">
-                          <i :class="member.icon" class="text-black text-2xl"></i>
-                        </div>
-                        <div class="mt-4 sm:ml-6 sm:mt-0 lg:ml-0 lg:mt-6">
-                          <h3 class="text-lg text-brand-primary font-semibold flex justify-center">{{ member.name
-                            }}</h3>
-                          <p class="mt-2 text-xs text-gray-500">{{ member.description }}</p>
-                          <i
-                            class="fas fa-arrow-right mt-5 font-bold text-2xl group-hover:text-brand-primary text-gray-300 duration-700"></i>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+
 
             </div>
           </div>
@@ -380,6 +355,33 @@ onMounted(() => {
                     </NuxtLink>
                   </figcaption>
                 </figure>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white !font-sans container my-5">
+            <div class="mx-auto max-w-7xl py-5 sm:px-2 lg:px-4">
+              <div class="mx-auto max-w-7xl px-4 lg:max-w-none">
+                <div class="max-w-7xl justify-between content-center">
+                  <h2 class="text-3xl font-semibold tracking-tight text-gray-900 text-center">
+                    Join Our Team
+                  </h2>
+                </div>
+                <div class="mt-5 grid grid-cols-1 gap-x-2 gap-y-4 lg:grid-cols-3 max-w-6xl mx-auto">
+                  <div v-for="member in joinAsMembers" :key="member.name"
+                    class="group button-animation sm:flex lg:block bg-gray-50 px-5 py-2 rounded-xs border-t-8  border-brand-primary">
+                    <div class="sm:flex-shrink-0 flex justify-center">
+                      <i :class="member.icon" class="text-black text-2xl"></i>
+                    </div>
+                    <div class="mt-4 sm:ml-6 sm:mt-0 lg:ml-0 lg:mt-6">
+                      <h3 class="text-lg text-brand-primary font-semibold flex justify-center">{{ member.name
+                        }}</h3>
+                      <p class="mt-2 text-xs text-gray-500">{{ member.description }}</p>
+                      <i
+                        class="fas fa-arrow-right mt-5 font-bold text-2xl group-hover:text-brand-primary text-gray-300 duration-700"></i>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
