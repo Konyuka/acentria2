bc34b40a442ffccfe8c67b0c84ed5018b0403620
Konyuka <michaelsaiba84@gmail.com>
2024-12-11 16:38:53 +0300

    push

--- pages/Insurance-claims.vue
+++ pages/Insurance-claims.vue
@@ -58,12 +58,11 @@ const exploreAviation = ref({
 
 <template>
     <section class="bg-white">
-
         <BannerMain :content="bannerContent" />
 
         <div class="bg-white w-full h-full">
             <div class="container py-5 max-w-6xl mx-auto font-sans text-black mt-2">
-                <div class="mx-auto  mb-3 font-sans">
+                <div class="mx-auto mb-3 font-sans">
                     <p class="mt-2 heading-class text-black">
                         Welcome to an easy <span class="text-brand-primary">claim experience</span>
                     </p>
@@ -73,19 +72,19 @@ const exploreAviation = ref({
                     can make claims on your insurance policy by filling out the form below. You can also track the
                     status of your claim by logging into your account. If you have any questions about your claim,
                     please <span class="underline text-brand-primary">contact us</span>. We are here to help you with
-                    your insurance needs. Thank you for
-                    choosing Acentria Insurance. We look forward to serving you.
-
-                <div class="mt-5"><a href="/meet-the-team"
-                        class="!button-animation bg-black !hover:bg-brand-primary text-white font-medium py-1 px-12 text-sm rounded-sm mt-10">
-                        Report or View a Claim <i class="fas fa-arrow-right text-primary ml-2"></i></a>
-                </div>
+                    your insurance needs. Thank you for choosing Acentria Insurance. We look forward to serving you.
                 </p>
+                <div class="mt-5">
+                    <a href="/meet-the-team"
+                        class="button-animation bg-black hover:bg-brand-primary text-white font-medium py-1 px-12 text-sm rounded-sm mt-10">
+                        Report or View a Claim <i class="fas fa-arrow-right text-primary ml-2"></i>
+                    </a>
+                </div>
             </div>
         </div>
 
         <div class="container">
-            <div class="mx-auto  pb-3 font-sans">
+            <div class="mx-auto pb-3 font-sans">
                 <p class="mt-2 subheading-class text-black">
                     Select a product to view the insurance claims process
                 </p>
@@ -93,20 +92,19 @@ const exploreAviation = ref({
             </div>
         </div>
 
-        <div class=" w-full h-full container mx-auto max-w-6xl">
-            <div class=" !font-sans  my-0">
+        <div class="w-full h-full container mx-auto max-w-6xl">
+            <div class="font-sans my-0">
                 <div class="py-10 sm:px-2 lg:px-4">
                     <div class="lg:max-w-none">
                         <div class="mt-5 grid grid-cols-1 gap-x-2 gap-y-4 lg:grid-cols-3">
                             <NuxtLink :href="member.link" v-for="member in joinAsMembers" :key="member.name"
-                                class="relative group button-animation sm:flex lg:block bg-gray-50 px-5 py-2 rounded-xs border-t-8  border-brand-primary">
+                                class="relative group button-animation sm:flex lg:block bg-gray-50 px-5 py-2 rounded-xs border-t-8 border-brand-primary">
                                 <div class="sm:flex-shrink-0 flex justify-center">
                                     <i :class="member.icon" class="text-black text-2xl"></i>
                                 </div>
                                 <div class="mt-4 sm:ml-6 sm:mt-0 lg:ml-0 lg:mt-6">
-                                    <h3 class="text-md text-brand-primary flex justify-center">{{ member.name
-                                        }}</h3>
-                                    <p class="mt-2 text-[12px] text-gray-500">{{ member.description }}</p>
+                                    <h3 class="text-lg text-brand-primary flex justify-center">{{ member.name }}</h3>
+                                    <p class="mt-2 text-md text-gray-500">{{ member.description }}</p>
                                 </div>
                                 <i class="absolute fas fa-angle-right right-5"></i>
                             </NuxtLink>
@@ -117,7 +115,6 @@ const exploreAviation = ref({
         </div>
 
         <ContentPic :content="exploreAviation" />
-
     </section>
 </template>
 
