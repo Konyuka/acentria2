417307e18d808c6858208efc1fb2b5b75a622858
Konyuka <michaelsaiba84@gmail.com>
2024-12-12 18:45:47 +0300

    push

--- pages/esg.vue
+++ pages/esg.vue
@@ -40,36 +40,31 @@ const bannerContent = ref({
         </div>
 
 
-        <div data-v-02281a80="" class="font-sans py-2 text-white" >
+        <div data-v-02281a80="" class="font-sans py-2 text-white">
 
-            <div class="container mx-auto flex px-4 lg:flex-row flex-col my-8 lg:my-4 lg:gap-16"
-                data-v-inspector="components/Why.vue:8:9">
-                <div class="relative p-4 lg:basis-1/2" data-v-inspector="components/Why.vue:9:13">
-                    <NuxtImg alt="Image of Elevate strategic risk management " loading="lazy" width="500" height="350"
+            <div class="container mx-auto flex px-4 lg:flex-row flex-col my-8 lg:my-4 lg:gap-16">
+                <div class="relative p-4 lg:basis-1/2">
+                    <NuxtImg alt="Image of Elevate strategic risk management" loading="lazy" width="500" height="350"
                         decoding="async" data-nimg="1" style="color: transparent; object-fit: contain"
-                        class="rounded-lg" src="https://acentriagroup.com/wp-content/uploads/2023/12/12-1024x625.jpg"
-                        data-v-inspector="components/Why.vue:10:17" />
+                        class="rounded-lg" src="https://acentriagroup.com/wp-content/uploads/2023/12/12-1024x625.jpg" />
                 </div>
-                <div class="flex flex-col justify-center lg:basis-1/2" data-v-inspector="components/Why.vue:14:13">
-
-                    <h2 class="subheading-font pb-4 leading-normal text-black">
+                <div class="flex flex-col justify-center lg:basis-1/2">
+                    <h2 class="text-xl font-semibold pb-4 leading-normal text-black">
                         Environmental Stewardship
                     </h2>
                     <div class="text-light-content pb-5 md:pb-6">
-                        <p class="textt-xs leading-normal text-black mb-4">
-                            We recognize the importance of environmental sustainability in
-                            preserving our planet for future generations. Acentria Group is dedicated to minimizing our
-                            environmental footprint through initiatives such as:
-                        <div class="text-xs mx-10">
-                            <span class="font-semibold mr-2 text-4xl pb-2">.</span>
+                        <p class="text-xs leading-normal text-black mb-4">
+                            We recognize the importance of environmental sustainability in preserving our planet for
+                            future generations. Acentria Group is dedicated to minimizing our environmental footprint
+                            through initiatives such as:
+                        <div class="text-xs mx-10 text-black">
+                            <span class="font-semibold mr-2 text-2xl pb-2">.</span>
                             Implementing energy-efficient practices in our offices and operations.
                             <br>
-
-                            <span class="font-semibold mr-2 text-4xl pb-2">.</span>
+                            <span class="font-semibold mr-2 text-2xl pb-2">.</span>
                             Promoting recycling and waste reduction programs.
                             <br>
-
-                            <span class="font-semibold mr-2 text-4xl pb-2">.</span>
+                            <span class="font-semibold mr-2 text-2xl pb-2">.</span>
                             Supporting renewable energy sources and reducing greenhouse gas emissions.
                             <br>
                         </div>
@@ -78,32 +73,29 @@ const bannerContent = ref({
                 </div>
             </div>
 
-            <div class="container mx-auto flex px-4 lg:flex-row-reverse flex-col my-8 lg:my-4 lg:gap-16"
-                data-v-inspector="components/Why.vue:32:9">
-                <div class="relative p-4 lg:basis-1/2 content-center" data-v-inspector="components/Why.vue:33:13">
+            <div class="container mx-auto flex px-4 lg:flex-row-reverse flex-col my-8 lg:my-4 lg:gap-16">
+                <div class="relative p-4 lg:basis-1/2 content-center">
                     <NuxtImg alt="Image of Enable growth" loading="lazy" width="500" height="350" decoding="async"
                         data-nimg="1" style="color: transparent; object-fit: contain" class="rounded-lg"
-                        src="https://acentriagroup.com/wp-content/uploads/2023/12/13.jpg"
-                        data-v-inspector="components/Why.vue:34:17" />
+                        src="https://acentriagroup.com/wp-content/uploads/2023/12/13.jpg" />
                 </div>
-                <div class="flex flex-col justify-center lg:basis-1/2" data-v-inspector="components/Why.vue:38:13">
-                    <h2 class="subheading-font pb-4 leading-normal text-black"
-                        data-v-inspector="components/Why.vue:18:17">
-                        Social Responsibility:
+                <div class="flex flex-col justify-center lg:basis-1/2">
+                    <h2 class="text-xl font-semibold pb-4 leading-normal text-black">
+                        Social Responsibility
                     </h2>
-                    <div class="text-light-content pb-5 md:pb-6" data-v-inspector="components/Why.vue:22:17">
-                        <p class="textt-xs leading-normal text-black mb-4" data-v-inspector="components/Why.vue:23:21">
-                            Acentria Group is committed to making a positive impact on the communities
-                            where we operate and beyond. Our social responsibility initiatives include: <br>
-
-                            <span class="font-semibold mr-2 text-4xl pb-2">.</span>
-                            Investing in community development projects that promote education, healthcare, and
-                            economic empowerment.
+                    <div class="text-light-content pb-5 md:pb-6">
+                        <p class="text-xs leading-normal text-black mb-4">
+                            Acentria Group is committed to making a positive impact on the communities where we operate
+                            and beyond. Our social responsibility initiatives include:
                             <br>
-                            <span class="font-semibold mr-2 text-4xl pb-2">.</span>
+                            <span class="font-semibold mr-2 text-2xl pb-2">.</span>
+                            Investing in community development projects that promote education, healthcare, and economic
+                            empowerment.
+                            <br>
+                            <span class="font-semibold mr-2 text-2xl pb-2">.</span>
                             Supporting diversity and inclusion within our workforce and supplier network.
                             <br>
-                            <span class="font-semibold mr-2 text-4xl pb-2">.</span>
+                            <span class="font-semibold mr-2 text-2xl pb-2">.</span>
                             Engaging in philanthropic activities and volunteering efforts to address societal
                             challenges.
                             <br>
@@ -112,83 +104,69 @@ const bannerContent = ref({
                 </div>
             </div>
 
-            <div class="container mx-auto flex px-4 lg:flex-row flex-col my-8 lg:my-4 lg:gap-16"
-                data-v-inspector="components/Why.vue:54:9">
-                <div class="relative p-4 lg:basis-1/2 content-center" data-v-inspector="components/Why.vue:55:13">
+            <div class="container mx-auto flex px-4 lg:flex-row flex-col my-8 lg:my-4 lg:gap-16">
+                <div class="relative p-4 lg:basis-1/2 content-center">
                     <NuxtImg alt="Image of Ensure regulatory compliance" loading="lazy" width="500" height="350"
                         decoding="async" data-nimg="1" style="color: transparent; object-fit: contain"
-                        class="rounded-lg"
-                        src="https://images.pexels.com/photos/761297/pexels-photo-761297.jpeg?auto=compress&cs=tinysrgb&w=600"
-                        data-v-inspector="components/Why.vue:56:17" />
+                        class="rounded-lg" src="/img/leadership.jpg" />
                 </div>
-                <div class="flex flex-col justify-center lg:basis-1/2" data-v-inspector="components/Why.vue:60:13">
-                    <h2 class="subheading-font pb-4 leading-normal text-black"
-                        data-v-inspector="components/Why.vue:18:17">
-                        Governance Excellence:
+                <div class="flex flex-col justify-center lg:basis-1/2">
+                    <h2 class="text-xl font-semibold pb-4 leading-normal text-black">
+                        Governance Excellence
                     </h2>
-                    <div class="text-light-content pb-5 md:pb-6" data-v-inspector="components/Why.vue:22:17">
-                        <p class="textt-xs leading-normal text-black mb-4" data-v-inspector="components/Why.vue:23:21">
-                            We uphold the highest standards of corporate governance to ensure
-                            transparency, accountability, and ethical conduct in all our business practices. Our
-                            governance
-                            framework includes: <br>
-
-                            <span class="font-semibold mr-2 text-4xl pb-2">.</span>
-                            Maintaining a diverse and independent board of directors that reflects our stakeholders&#39;
+                    <div class="text-light-content pb-5 md:pb-6">
+                        <p class="text-xs leading-normal text-black mb-4">
+                            We uphold the highest standards of corporate governance to ensure transparency,
+                            accountability, and ethical conduct in all our business practices. Our governance framework
+                            includes:
+                            <br>
+                            <span class="font-semibold mr-2 text-2xl pb-2">.</span>
+                            Maintaining a diverse and independent board of directors that reflects our stakeholders'
                             interests.
                             <br>
-
-                            <span class="font-semibold mr-2 text-4xl pb-2">.</span>
+                            <span class="font-semibold mr-2 text-2xl pb-2">.</span>
                             Conducting regular audits and risk assessments to uphold compliance with regulatory
                             requirements.
                             <br>
-
-                            <span class="font-semibold mr-2 text-4xl pb-2">.</span>
+                            <span class="font-semibold mr-2 text-2xl pb-2">.</span>
                             Promoting a culture of integrity and ethical behavior among our employees and partners.
                             <br>
-
                         </p>
                     </div>
                 </div>
             </div>
 
-            <div class="container mx-auto flex px-4 lg:flex-row-reverse flex-col my-8 lg:my-4 lg:gap-16"
-                data-v-inspector="components/Why.vue:32:9">
-                <div class="relative p-4 lg:basis-1/2 content-center" data-v-inspector="components/Why.vue:33:13">
+            <div class="container mx-auto flex px-4 lg:flex-row-reverse flex-col my-8 lg:my-4 lg:gap-16">
+                <div class="relative p-4 lg:basis-1/2 content-center">
                     <NuxtImg alt="Image of Enable growth" loading="lazy" width="500" height="350" decoding="async"
                         data-nimg="1" style="color: transparent; object-fit: contain" class="rounded-lg"
-                        src="https://acentriagroup.com/wp-content/uploads/2023/12/13.jpg"
-                        data-v-inspector="components/Why.vue:34:17" />
+                        src="/img/handshake.jpg" />
                 </div>
-                <div class="flex flex-col justify-center lg:basis-1/2" data-v-inspector="components/Why.vue:38:13">
-                    <h2 class="subheading-font pb-4 leading-normal text-black"
-                        data-v-inspector="components/Why.vue:18:17">
+                <div class="flex flex-col justify-center lg:basis-1/2">
+                    <h2 class="text-xl font-semibold pb-4 leading-normal text-black">
                         Economic Responsibility
                     </h2>
-                    <div class="text-light-content pb-5 md:pb-6" data-v-inspector="components/Why.vue:22:17">
-                        <p class="textt-xs leading-normal text-black mb-4" data-v-inspector="components/Why.vue:23:21">
-                            Acentria Group strives to contribute to economic development
-                            by creating value for our stakeholders, promoting responsible growth, and providing
-                            innovative solutions that support financial stability and prosperity. These are some of our
-                            initiatives <br>
-
-                            <span class="font-semibold mr-2 text-4xl pb-2">.</span>
-                            Supporting small and medium-sized enterprises (SMEs) by providing tailored
-                            insurance products and financial services that promote their growth and
-                            resilience.
+                    <div class="text-light-content pb-5 md:pb-6">
+                        <p class="text-xs leading-normal text-black mb-4">
+                            Acentria Group strives to contribute to economic development by creating value for our
+                            stakeholders, promoting responsible growth, and providing innovative solutions that support
+                            financial stability and prosperity. These are some of our initiatives:
                             <br>
-                            <span class="font-semibold mr-2 text-4xl pb-2">.</span>
-                            Investing in local communities by creating job opportunities and fostering
-                            economic empowerment through skills development programs.
+                            <span class="font-semibold mr-2 text-2xl pb-2">.</span>
+                            Supporting small and medium-sized enterprises (SMEs) by providing tailored insurance
+                            products and financial services that promote their growth and resilience.
                             <br>
-                            <span class="font-semibold mr-2 text-4xl pb-2">.</span>
-                            Contributing to financial literacy initiatives that educate individuals and
-                            businesses on the importance of risk management and insurance in securing
-                            their future.
+                            <span class="font-semibold mr-2 text-2xl pb-2">.</span>
+                            Investing in local communities by creating job opportunities and fostering economic
+                            empowerment through skills development programs.
                             <br>
-                            <span class="font-semibold mr-2 text-4xl pb-2">.</span>
-                            Prioritizing long-term sustainable business practices that drive profitability
-                            while ensuring positive socio-economic impacts.
+                            <span class="font-semibold mr-2 text-2xl pb-2">.</span>
+                            Contributing to financial literacy initiatives that educate individuals and businesses on
+                            the importance of risk management and insurance in securing their future.
+                            <br>
+                            <span class="font-semibold mr-2 text-2xl pb-2">.</span>
+                            Prioritizing long-term sustainable business practices that drive profitability while
+                            ensuring positive socio-economic impacts.
                             <br>
                         </p>
                     </div>
