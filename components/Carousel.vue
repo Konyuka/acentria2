79e8a4ec605e895f0c2ac42bd6711559138a4b4a
Konyuka <michaelsaiba84@gmail.com>
2024-12-31 17:13:16 +0300

    push

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="!overflow-x-hidden font-sans bg-white">
+    <div class="!overflow-x-hidden !font-sans bg-white">
         <Carousel :autoplay="2000" :wrap-around="true">
             <Slide v-for="slide in slides" :key="slide" class="h-screen">
                 <NuxtImg :src="slide.imageUrl" class="w-full h-full object-cover" />
                 <div class="absolute inset-0 bg-gradient-to-t from-gray-700 to-transparent"></div>
                 <div
                     class="absolute top-1/2 mx-10 md:mx-0 md:left-1/2 transform md:-translate-x-1/2 -translate-y-1/2 text-center px-0 text-white">
-                    <h2 class="text-6xl font-bold mb-4 tracking-widest text-black" v-html="slide.title"></h2>
+                    <h2 class="text-6xl font-semibold mb-4 tracking-widest text-black" v-html="slide.title"></h2>
                     <p class="text-base sm:text-lg md:text-2xl lg:text-2xl mb-8 mt-2">{{ slide.description }}</p>
                     <NuxtLink :to="slide.url"
                         class="button-animation z-[51] relative bg-black !hover:bg-black text-white font-medium py-3 px-6 sm:px-8 md:px-10 text-xl sm:text-sm rounded-sm mt-20">
