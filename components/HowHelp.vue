e1d6b2e021c7b5b76e0337c9b69f8e09eb89b165
Konyuka <michaelsaiba84@gmail.com>
2025-01-06 15:40:55 +0300

    push

--- components/HowHelp.vue
+++ components/HowHelp.vue
@@ -12,30 +12,31 @@
 
                 <div>
                     <div class="max-w-6xl mx-auto py-10">
-                        <h2 class="text-2xl font-bold text-gray-800 mb-6">I need to:</h2>
                         <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                             <!-- Request Support -->
-                            <div class="p-6 border rounded-lg shadow-sm hover:shadow-md transition-shadow">
+                            <div class="button-animation p-6 border rounded-lg shadow-sm hover:shadow-md ">
                                 <div class="flex items-center mb-4">
-                                    <i class="fas fa-info text-brnad-"></i>
-                                    <h3 class="ml-4 text-lg font-semibold text-gray-900">Request Support</h3>
+                                    <i class="fas fa-question text-brand-primary text-2xl"></i>
+                                    <h3 class="ml-4 text-lg font-semibold text-gray-900">
+                                        Request Support <i class="fas fa-arrow-right ml-4"></i>
+                                    </h3>
                                 </div>
-                                <p class="text-gray-600">
+                                <p class="text-black">
                                     Get assistance with insurance claims, certificates, account or other support topics.
                                 </p>
-                                
+
                             </div>
 
                             <!-- Make a Business Inquiry -->
-                            <div class="p-6 border rounded-lg shadow-sm hover:shadow-md transition-shadow">
+                            <div
+                                class="button-animation p-6 border rounded-lg shadow-sm hover:shadow-md">
                                 <div class="flex items-center mb-4">
-                                    <div
-                                        class="w-10 h-10 flex justify-center items-center rounded-full border border-red-500 text-red-500">
-                                        👥
-                                    </div>
-                                    <h3 class="ml-4 text-lg font-semibold text-gray-900">Make a Business Inquiry</h3>
+                                    <i class="fas fa-briefcase text-brand-primary text-2xl"></i>
+                                    <h3 class="ml-4 text-lg font-semibold text-gray-900">
+                                        Make a Business Inquiry <i class="fas fa-arrow-right ml-4"></i>
+                                    </h3>
                                 </div>
-                                <p class="text-gray-600">
+                                <p class="text-black">
                                     Tell us more about you and how we can help your business make better decisions.
                                 </p>
                             </div>
