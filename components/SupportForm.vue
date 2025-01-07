2108260447d5a0c1101688e5c748abc9378dd664
Konyuka <michaelsaiba84@gmail.com>
2025-01-07 07:43:44 +0300

    push

--- components/SupportForm.vue
+++ components/SupportForm.vue
@@ -1,5 +1,5 @@
 <script setup>
-const stage = ref(1)
+const stage = ref(6)
 
 const setStage = (stage) => {
     stage.value = stage
@@ -25,7 +25,7 @@ const setStage = (stage) => {
                     <input id="food" type="text" placeholder="mail@example.com" />
                 </div>
                 <div class="my-3">
-                    <p class="text-md italic">So we can get in touch with you promptly.</p>
+                    <p class="text-md italic text-gray-600">So we can get in touch with you promptly.</p>
                 </div>
                 <div>
                     <button @click="setStage(2)"
@@ -37,77 +37,238 @@ const setStage = (stage) => {
             </div>
 
             <div v-show="stage == 2" class="flex flex-col gap-2">
-                <div class="mat-input ">
-                    <label for="food">Your Name</label>
-                    <input id="food" type="text" placeholder="John Doe" />
-                </div>
-
-                <div class="select">
-                    <div class="selected" data-default="All" data-one="option-1" data-two="option-2"
-                        data-three="option-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" class="arrow">
-                            <path
-                                d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z">
-                            </path>
-                        </svg>
-                    </div>
-                    <div class="options">
-                        <div title="all">
-                            <input id="all" name="option" type="radio" checked="" />
-                            <label class="option" for="all" data-txt="All"></label>
-                        </div>
-                        <div title="finance">
-                            <input id="finance" name="option" type="radio" />
-                            <label class="option" for="finance" data-txt="Finance"></label>
-                        </div>
-                        <div title="claims">
-                            <input id="claims" name="option" type="radio" />
-                            <label class="option" for="claims" data-txt="Claims"></label>
-                        </div>
-                        <div title="client-relations">
-                            <input id="client-relations" name="option" type="radio" />
-                            <label class="option" for="client-relations" data-txt="Client Relations"></label>
-                        </div>
-                        <div title="media-relations">
-                            <input id="media-relations" name="option" type="radio" />
-                            <label class="option" for="media-relations" data-txt="Media Relations"></label>
-                        </div>
-                        <div title="actuarial">
-                            <input id="actuarial" name="option" type="radio" />
-                            <label class="option" for="actuarial" data-txt="Actuarial"></label>
-                        </div>
-                        <div title="reinsurance">
-                            <input id="reinsurance" name="option" type="radio" />
-                            <label class="option" for="reinsurance" data-txt="Reinsurance"></label>
-                        </div>
-                        <div title="technology">
-                            <input id="technology" name="option" type="radio" />
-                            <label class="option" for="technology" data-txt="Technology"></label>
-                        </div>
-                        <div title="investment">
-                            <input id="investment" name="option" type="radio" />
-                            <label class="option" for="investment" data-txt="Investment"></label>
-                        </div>
-                        <div title="other">
-                            <input id="other" name="option" type="radio" />
-                            <label class="option" for="other" data-txt="Other"></label>
-                        </div>
-                    </div>
-
-                </div>
-
-
                 <div class="my-3">
-                    <p class="text-md italic">So that we can channel your issue to the right support</p>
+                    <p class="text-md italic text-gray-600">
+                        Please input your target department
+                        <br />
+                        Helps channel your issue to the right support
+                    </p>
                 </div>
+                <div class="flex gap-20 ">
+
+
+                    <div class="select">
+                        <div class="selected" data-default="All" data-finance="Finance" data-claims="Claims"
+                            data-client-relations="Client Relations" data-media-relations="Media Relations"
+                            data-actuarial="Actuarial" data-reinsurance="Reinsurance" data-technology="Technology"
+                            data-investment="Investment" data-other="Other" data-three="option-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" class="arrow">
+                                <path
+                                    d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z">
+                                </path>
+                            </svg>
+                        </div>
+                        <div class="options">
+                            <div title="all">
+                                <input id="all" name="option" type="radio" checked="" />
+                                <label class="option" for="all" data-txt="All"></label>
+                            </div>
+                            <div title="finance">
+                                <input id="finance" name="option" type="radio" />
+                                <label class="option" for="finance" data-txt="Finance"></label>
+                            </div>
+                            <div title="claims">
+                                <input id="claims" name="option" type="radio" />
+                                <label class="option" for="claims" data-txt="Claims"></label>
+                            </div>
+                            <div title="client-relations">
+                                <input id="client-relations" name="option" type="radio" />
+                                <label class="option" for="client-relations" data-txt="Client Relations"></label>
+                            </div>
+                            <div title="media-relations">
+                                <input id="media-relations" name="option" type="radio" />
+                                <label class="option" for="media-relations" data-txt="Media Relations"></label>
+                            </div>
+                            <div title="actuarial">
+                                <input id="actuarial" name="option" type="radio" />
+                                <label class="option" for="actuarial" data-txt="Actuarial"></label>
+                            </div>
+                            <div title="reinsurance">
+                                <input id="reinsurance" name="option" type="radio" />
+                                <label class="option" for="reinsurance" data-txt="Reinsurance"></label>
+                            </div>
+                            <div title="technology">
+                                <input id="technology" name="option" type="radio" />
+                                <label class="option" for="technology" data-txt="Technology"></label>
+                            </div>
+                            <div title="investment">
+                                <input id="investment" name="option" type="radio" />
+                                <label class="option" for="investment" data-txt="Investment"></label>
+                            </div>
+                            <div title="other">
+                                <input id="other" name="option" type="radio" />
+                                <label class="option" for="other" data-txt="Other"></label>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div>
+                        <h1 @click="setStage(3)"
+                            class="mt-2 button-animation text-brand-primary group text-xl font-semibold">
+                            Proceed
+                            <i
+                                class="ml-3 text-[20px] group-hover:text-brand-primary fas fa-arrow-right text-gray-500"></i>
+                        </h1>
+                    </div>
+
+                </div>
+
+
+
+
+
+            </div>
+
+            <div v-show="stage == 3" class="flex flex-col gap-2">
+
+
+                <div class="mat-input ">
+                    <label for="description">Description of the Issue</label>
+                    <textarea id="description" type="textarea" placeholder="I need assitance on..." />
+                </div>
+                <div class="my-3">
+                    <p class="text-md italic text-gray-600">
+                        Please describe the problem or assistance you need.
+                    </p>
+                </div>
+
+
                 <div>
-                    <h1 @click="setStage(3)"
+                    <button @click="setStage(4)"
                         class="mt-2 button-animation text-brand-primary group text-xl font-semibold">
                         Proceed
                         <i class="ml-3 text-[20px] group-hover:text-brand-primary fas fa-arrow-right text-gray-500"></i>
-                    </h1>
+                    </button>
                 </div>
             </div>
+
+            <div v-show="stage == 4" class="flex flex-col gap-2">
+
+
+                <div class="mat-input ">
+                    <label for="food">Relevant Details</label>
+                    <textarea id="food" type="textarea" placeholder="You can paste links too..." />
+                </div>
+                <div class="my-3">
+                    <p class="text-md italic text-gray-600">
+                        Any specific information that might help us understand your situation better.
+                    </p>
+                </div>
+
+
+                <div>
+                    <button @click="setStage(5)"
+                        class="mt-2 button-animation text-brand-primary group text-xl font-semibold">
+                        Proceed
+                        <i class="ml-3 text-[20px] group-hover:text-brand-primary fas fa-arrow-right text-gray-500"></i>
+                    </button>
+                </div>
+            </div>
+
+            <div v-show="stage == 5" class="flex flex-col gap-2">
+                <div class="my-3">
+                    <p class="text-md italic text-gray-600">
+                        Please input your target department
+                        <br />
+                        Helps channel your issue to the right support
+                    </p>
+                </div>
+                <div class="flex gap-20 ">
+
+
+                    <div class="select">
+                        <div class="selected" data-default="All" data-finance="Finance" data-claims="Claims"
+                            data-client-relations="Client Relations" data-media-relations="Media Relations"
+                            data-actuarial="Actuarial" data-reinsurance="Reinsurance" data-technology="Technology"
+                            data-investment="Investment" data-other="Other" data-three="option-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" class="arrow">
+                                <path
+                                    d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z">
+                                </path>
+                            </svg>
+                        </div>
+                        <div class="options">
+                            <div title="all">
+                                <input id="all" name="option" type="radio" checked="" />
+                                <label class="option" for="all" data-txt="All"></label>
+                            </div>
+                            <div title="finance">
+                                <input id="finance" name="option" type="radio" />
+                                <label class="option" for="finance" data-txt="Finance"></label>
+                            </div>
+                            <div title="claims">
+                                <input id="claims" name="option" type="radio" />
+                                <label class="option" for="claims" data-txt="Claims"></label>
+                            </div>
+                            <div title="client-relations">
+                                <input id="client-relations" name="option" type="radio" />
+                                <label class="option" for="client-relations" data-txt="Client Relations"></label>
+                            </div>
+                            <div title="media-relations">
+                                <input id="media-relations" name="option" type="radio" />
+                                <label class="option" for="media-relations" data-txt="Media Relations"></label>
+                            </div>
+                            <div title="actuarial">
+                                <input id="actuarial" name="option" type="radio" />
+                                <label class="option" for="actuarial" data-txt="Actuarial"></label>
+                            </div>
+                            <div title="reinsurance">
+                                <input id="reinsurance" name="option" type="radio" />
+                                <label class="option" for="reinsurance" data-txt="Reinsurance"></label>
+                            </div>
+                            <div title="technology">
+                                <input id="technology" name="option" type="radio" />
+                                <label class="option" for="technology" data-txt="Technology"></label>
+                            </div>
+                            <div title="investment">
+                                <input id="investment" name="option" type="radio" />
+                                <label class="option" for="investment" data-txt="Investment"></label>
+                            </div>
+                            <div title="other">
+                                <input id="other" name="option" type="radio" />
+                                <label class="option" for="other" data-txt="Other"></label>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div>
+                        <h1 @click="setStage(6)"
+                            class="mt-2 button-animation text-brand-primary group text-xl font-semibold">
+                            Proceed
+                            <i
+                                class="ml-3 text-[20px] group-hover:text-brand-primary fas fa-arrow-right text-gray-500"></i>
+                        </h1>
+                    </div>
+
+                </div>
+
+
+
+
+
+            </div>
+
+            <div v-show="stage == 6" class="flex flex-col gap-2">
+
+
+
+                <div class="mt-20 flex flex-col justify-center items-center">
+                    <h1 class="text-lg font-semibold">
+                        We are committed to resolving your issues <span class="text-brand-primary">as quickly and efficiently as possible.</span>
+                    </h1>
+                    <p class="text-md italic text-gray-600">
+                        Thank you for choosing Acentria. We look forward to assisting you.
+                    </p>
+                </div>
+
+
+            </div>
+
+
+
+
 
 
 
@@ -120,7 +281,7 @@ const setStage = (stage) => {
 
 /* From Uiverse.io by 3bdel3ziz-T */
 .select {
-    width: 600px;
+    width: 400px;
     cursor: pointer;
     position: relative;
     transition: 300ms;
@@ -179,7 +340,7 @@ const setStage = (stage) => {
     padding: 5px;
     transition: 300ms;
     background-color: #2a2f3b;
-    width: 580px;
+    width: 380px;
     font-size: 15px;
 }
 
@@ -250,6 +411,7 @@ const setStage = (stage) => {
 
 
 
+
 .mat-input {
     position: relative;
     font-size: inherit;
@@ -268,8 +430,26 @@ const setStage = (stage) => {
     width: 600px
 }
 
+.mat-input textarea {
+    border-width: 0 0 1px;
+    outline: none;
+    background-color: #0000000a;
+    border-radius: 4px 4px 0 0;
+    padding: 1.8em 0.75em 0.75em;
+    border-color: #8c8c8c;
+    border-style: solid;
+    font-size: inherit;
+    transition: 0.1s;
+    width: 600px
+}
+
 .mat-input input:hover,
 .mat-input input:focus {
+    border-bottom-width: 2px;
+    padding-bottom: calc(0.75em - 1px);
+}
+.mat-input textarea:hover,
+.mat-input textarea:focus {
     border-bottom-width: 2px;
     padding-bottom: calc(0.75em - 1px);
 }
@@ -277,8 +457,13 @@ const setStage = (stage) => {
 .mat-input input:hover {
     border-color: #121212;
 }
-
 .mat-input input:focus {
+    border-color: #ed1c24;
+}
+.mat-input textarea:hover {
+    border-color: #121212;
+}
+.mat-input textarea:focus {
     border-color: #ed1c24;
 }
 
@@ -294,9 +479,17 @@ const setStage = (stage) => {
 .mat-input:has(input:focus) label {
     color: #ed1c24;
 }
+.mat-input:has(textarea:focus) label {
+    color: #ed1c24;
+}
 
 .mat-input:has(input:focus) label,
 .mat-input:not(:has(input:placeholder-shown)) label {
+    top: 0.75em;
+    font-size: 0.8em;
+}
+.mat-input:has(textarea:focus) label,
+.mat-input:not(:has(textarea:placeholder-shown)) label {
     top: 0.75em;
     font-size: 0.8em;
 }
@@ -306,8 +499,16 @@ const setStage = (stage) => {
     transition: 0.2s;
     color: #aaa;
 }
+.mat-input textarea::placeholder {
+    opacity: 0;
+    transition: 0.2s;
+    color: #aaa;
+}
 
 .mat-input input:focus::placeholder {
+    opacity: 1;
+}
+.mat-input textarea:focus::placeholder {
     opacity: 1;
 }
 </style>
