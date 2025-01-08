74fc52e59e39a5ca6a992f3c9da1884412737746
Konyuka <michaelsaiba84@gmail.com>
2025-01-08 10:16:03 +0300

    nav logo

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -172,7 +172,7 @@ onBeforeUnmount(() => {
                     <NuxtLink to="/">
                         <NuxtImg alt="Acentria" fetchPriority="high"  decoding="async"
                             data-nimg="1" class="mr-8 w-full h-16" style="color: transparent; object-fit: contain;"
-                            src="/img/Logo2.png" />
+                            src="/img/logo/png1.png" />
 
                         <!-- <NuxtImg alt="Acentria" fetchPriority="high" decoding="async"
                             data-nimg="1" class="mr-8 w-full h-14" style="color: transparent; object-fit: contain;"
