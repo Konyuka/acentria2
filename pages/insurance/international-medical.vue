f816deefcb7586625890df9da9c84ecbdc022a34
Konyuka <michaelsaiba84@gmail.com>
2025-01-09 11:25:28 +0300

    push

--- pages/insurance/international-medical.vue
+++ pages/insurance/international-medical.vue
@@ -10,38 +10,51 @@ const InsuranceBannerContent = ref({
 
 const faqs = ref([
     {
-        name: 'What is home insurance?',
-        content: 'Home insurance or Domestic package is a type of insurance that protects your residential house and its contents from unexpected events like fires, theft, or accidental damage. It can be extended to cover domestic workers from accidental death or injury and liabilities to third parties.',
+        name: 'What is international medical insurance?',
+        content: '<p>International medical insurance provides comprehensive health coverage for individuals and families who are living, working, or traveling abroad. It covers a wide range of medical services, including emergency care, hospitalization, outpatient treatments, and preventive care.</p>',
     },
     {
-        name: 'How often should I review and update my home insurance policy?',
-        content: 'Review of policy and declaration of covered items should be done whenever you make significant changes to your home or acquire valuable new possessions. This ensures your coverage remains adequate and full payout in case of a claim. Our agents can also contact you & share reminder notifications to your email / phone if you want to review and update your policy.',
+        name: 'Who needs international medical insurance?',
+        content: '<p>International medical insurance is ideal for expatriates, international students, business travelers, and anyone spending an extended period abroad. It ensures access to quality healthcare services regardless of location.</p>',
     },
     {
-        name: 'How is the value of my home and belongings determined for insurance purposes?',
-        content: 'The value of your home is typically determined by its replacement cost, which is how much it would cost to rebuild it if it were destroyed. The value of your belongings is based on their current market value or replacement cost, depending on your policy.',
+        name: 'What does international medical insurance cover?',
+        content: '<p>Coverage typically includes:</p><ul class="ml-5"><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Emergency medical care</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Hospitalization and surgeries</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Outpatient treatments and consultations</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Prescription medications</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Preventive care and vaccinations</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Dental and vision care (optional add-ons)</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Medical evacuation and repatriation</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Maternity care (optional add-ons)</li></ul>',
     },
     {
-        name: 'Is it important to declare my belongings during policy uptake?',
-        content: 'Yes, it is important to maintain and declare a clear schedule of your belonging during purchase of insurance to avoid under insurance. Under insurance is when the value of goods and quantities being insured is lower than actual value of goods leaving the policy holder responsible for a large percentage of total loss or expense in case of a claims and may lead to financial hardship.',
+        name: 'What is the difference between travel insurance and international medical insurance?',
+        content: '<p>Travel insurance is designed for short-term trips and typically covers trip cancellations, lost luggage, and emergency medical care. International medical insurance, on the other hand, provides long-term health coverage for individuals living or working abroad, covering a broader range of medical services and treatments.</p>',
     },
     {
-        name: 'Do I need home Insurance if I rent my home?',
-        content: 'Yes, even if you\'re renting, it\'s a good idea to have home insurance. It protects your personal belongings in case of theft or damage and can provide liability coverage if someone is injured while visiting.',
+        name: 'How do I choose the right international medical insurance plan?',
+        content: '<p>Consider the following factors:</p><ul class="ml-5"><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Coverage Needs: Assess your medical needs and the services you require.</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Network of Providers: Ensure the plan includes access to healthcare providers in your destination country.</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Policy Limits: Review coverage limits for medical expenses and treatments.</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Cost: Compare premiums, deductibles, and out-of-pocket costs.</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Additional Benefits: Look for added benefits such as medical evacuation, dental and vision care, and preventive services.</li></ul>',
     },
     {
-        name: 'How do I claim & how fast is the claims process?',
-        content: '<p>Our claims process is hassle free.</p><p>The claims process varies depending on the nature of cover. It is important to familiarize yourself with your policy coverage and necessary claims documents in the event of a claims.</p><NuxtLink href="/" class="button-animation bg-black !hover:bg-brand-primary text-white font-medium py-1 px-12 text-sm rounded-sm mt-10"> Launch a Claim <i class="fas fa-arrow-right text-primary ml-2"></i> </NuxtLink>',
+        name: 'How do I file a claim?',
+        content: '<p>Our claims process is hassle free.</p><p>To file a claim, follow these steps:</p><ol class="ml-5 list-decimal"><li>Contact Insurer: Notify your insurance provider as soon as possible.</li><li>Gather Documentation: Collect all necessary documents, such as medical bills, receipts, and claim forms.</li><li>Submit Claim: Submit the completed claim form and documentation to your insurance provider.</li></ol><p>It is important to familiarize yourself with your policy coverage and necessary claims documents in the event of a claims.</p><NuxtLink href="/" class="button-animation bg-black !hover:bg-brand-primary text-white font-medium py-1 px-12 text-sm rounded-sm mt-10"> Launch a Claim <i class="fas fa-arrow-right text-primary ml-2"></i> </NuxtLink>',
     },
     {
-        name: 'What are the exclusions in Home Insurance?',
-        content: '<p>Some of the exclusions in Home Insurance include:</p><ul class="ml-5"><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Damage resulting from wear and tear i.e. plumbing works, electrical works.</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Insured engaging in illegal activities.</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Acts of God such as earthquake & floods damage unless their special extensions.</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Pest infestations, mold and mildew and sewer backup.</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> War, including nuclear, biological, or chemical attacks.</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Intentionally caused by the homeowner or residents.</li><li><i class="fas fa-caret-right text-brand-primary mr-1"></i> Government actions, such as property confiscation or demolition.</li></ul><p>It is important to read the policy document carefully to understand the exclusions that apply. In case of any concerns, feel free to engage our team of experts for advice.</p>',
+        name: 'Is maternity care covered?',
+        content: '<p>Maternity care is often available as an optional add-on to international medical insurance plans. Coverage may include prenatal care, delivery, and postnatal care. Be sure to check the specific terms and conditions of your policy.</p>',
+    },
+    {
+        name: 'What is medical evacuation and repatriation?',
+        content: '<p>Medical evacuation involves transporting you to the nearest medical facility equipped to handle your condition. Repatriation refers to returning you to your home country for continued medical treatment if necessary. These services are typically covered under international medical insurance plans.</p>',
+    },
+    {
+        name: 'Can I include my family in the plan?',
+        content: '<p>Yes, our international medical insurance plans offer family coverage, allowing you to include your spouse and dependents under a single policy.</p>',
+    },
+    {
+        name: 'How can I access healthcare services abroad?',
+        content: '<p>Our international medical insurance plans provide access to a network of healthcare providers worldwide. You can use our online portal or contact our customer service for assistance in finding and accessing healthcare services in your destination country.</p>',
     },
     {
         name: 'How can I get started?',
         content: '<p>Our streamlined enrollment process makes it simple to get the coverage you need.</p><p>You can compare plans, get quotes, and enroll online within minutes - <a href="#" class="font-bold italic text-brand-primary">Do it Yourself</a> or Review your quotes online, then get tailored advice from our agents - <a href="#" class="font-bold italic text-brand-primary">Engage our expert</a>.</p><p>Our customer service team is available to assist you with any questions or concerns.</p>',
     },
-])
+]);
+
 
 const intro = ref('At Acentria, we’re not just about insurance – we understand what’s on your mind. Our easy process takes care of your insurance needs, offering reliable coverage that gives you peace of mind.Let us handle your insurance worries at Acentria.')
 const leftTitle = ref(`Why Insure with <br> <span class="text-brand-primary">Acentria</span>`)
@@ -82,7 +95,22 @@ const benefits = ref([
     {
         name: 'Trusted Provider',
         icon: 'fas fa-shield-alt',
-        description: 'With years of experience and a strong reputation, we are a trusted name in the insurance industry.',
+        description: 'With years of experience and a strong reputation, we are a trusted name in the international medical insurance industry.',
+    },
+    {
+        name: 'Customer Focus',
+        icon: 'fas fa-users',
+        description: 'We prioritize our customers\' well-being, offering personalized service and prompt claims processing.',
+    },
+    {
+        name: 'Affordable Plans',
+        icon: 'fas fa-money-bill-wave',
+        description: 'We offer a range of plans to fit different budgets and healthcare needs.',
+    },
+    {
+        name: 'Network of Providers',
+        icon: 'fas fa-network-wired',
+        description: 'Access to an extensive network of healthcare providers, ensuring you receive the best care possible.',
     },
     {
         name: 'Customer Satisfaction',
@@ -92,19 +120,19 @@ const benefits = ref([
     {
         name: 'Easy and Hassle-Free',
         icon: 'fas fa-check-circle',
-        description: 'Purchasing home insurance should be quick and easy. Our online platform allows you to compare plans, get quotes, and purchase coverage in just a few clicks. Plus, our mobile app makes it convenient to access your policy information and file claims on the go.',
+        description: 'Purchasing health insurance should be quick and easy. Our online platform allows you to compare plans, get quotes, and purchase coverage in just a few clicks. Plus, our mobile app makes it convenient to access your policy information and file reimbursement claims on the go.',
     },
     {
-        name: 'Customized Plans',
-        icon: 'fas fa-cogs',
-        description: 'Tailor your coverage to fit your unique needs and financial goals.',
+        name: '24/7 Assistance',
+        icon: 'fas fa-headset',
+        description: 'Our dedicated support team is available around the clock to assist you with any health-related issues.',
     },
-    
-])
+]);
+
 
 
 const whyTheInsurance = ref({
-    title: 'Why <span class="text-brand-primary">International Medical Insurance</span>',
+    title: 'Why Choose <span class="text-brand-primary">International Medical Insurance</span>',
     description: 'Traveling or living abroad can be an exciting adventure, but it\'s essential to have the right medical coverage to protect yourself and your loved ones. Our international medical insurance provides extensive coverage, ensuring that you have access to quality healthcare wherever you are in the world.',
 });
 
@@ -193,7 +221,7 @@ const getStarted = ref({
         <div class="container mx-auto px-4 py-2 mt-5">
             <h1 class="text-2xl font-semibold mb-4">Frequently <span class="text-brand-primary">Asked Questions</span>
             </h1>
-            <p class="text-sm leading-normal text-gray-700">We have all the answers to your questions.</p>
+            <p class="text-sm leading-normal text-gray-700 font-semibold">We have all the answers to your questions.</p>
             <Accordion :faqs="faqs" />
         </div>
 
